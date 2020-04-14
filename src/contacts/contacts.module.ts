f2ec5271a5ef12ef654ef4beb9f9837598b32f45
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-04-14 17:17:42 -0300

    add contact entity

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts/contacts.service';
 import { ContactsController } from './contacts/contacts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './contacts/contact.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contact])],
   providers: [ContactsService],
-  controllers: [ContactsController]
+  controllers: [ContactsController],
 })
 export class ContactsModule {}
